a537f60a4763297da1d1ddcb3c565642a6cde605
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-10-19 08:55:57 +0700

    change base of defineConfig

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://leanhvu21042001.github.io/example-vite-project",
+  base: "/example-vite-project/",
 });
