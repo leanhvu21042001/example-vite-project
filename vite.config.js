7d7d22f1bf5d70ff8acdf6c52ffd09afcd59147d
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-10-19 09:22:30 +0700

    setup ssr

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/example-vite-project/",
+  build: {
+    minify: false,
+  },
+  // legacy: {
+  //   buildSsrCjsExternalHeuristics: true,
+  // },
 });
